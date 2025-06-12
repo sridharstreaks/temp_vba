800840f713bbfb29c70a30ee64ef594e181f47ae
Sridhar Streaks <100800059+sridharstreaks@users.noreply.github.com>
2025-06-12 15:36:45 +0530

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -1,8 +1,16 @@
 awk '
-  /<util:set id="some_word"/ { in=1 }
-  in && /<value>/ {
-    gsub(/.*<value>|<\/value>.*/, "", $0)
-    printf "\"%s\",", $0
+  /<util:set[[:space:]]+id="some_word"/ { in_block=1; next }
+  /<\/util:set>/          { in_block=0 }
+  in_block && /<value>/  {
+    # extract just the text between <value>…</value>
+    if ( match($0, /<value>([^<]+)<\/value>/, m) ) {
+      printf "\"%s\",", m[1]
+    }
   }
-  /<\/util:set>/ { in=0 }
+  END {
+    # strip trailing comma and add newline
+    sub(/,$/, "", out = "")
+    # but since we printed directly, re-print everything from stdout
+    # instead, you can pipe through sed as shown below
+  }
 ' file.xml | sed 's/,$//'
