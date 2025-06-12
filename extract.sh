b22a47d0c6c1b601511ba4c73e84985350bb9948
Sridhar Streaks <100800059+sridharstreaks@users.noreply.github.com>
2025-06-13 00:28:33 +0530

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -1,24 +1,15 @@
-#!/usr/bin/env bash
-# file: extract_values_simple.sh
-
-XML_FILE=$1
-ID=$2
-
-awk -v id="$ID" '
-  # when we hit the <util:set id"…" line, start collecting…
-  $0 ~ "<util:set id\"" id "\"" { inblock=1; next }
-  # …until the closing tag
-  $0 ~ "</util:set>"      { inblock=0 }
-  # inside block, look for <value>…</value>
-  inblock && match($0, /<value>([^<]+)<\/value>/, m) {
-    vals[++n] = "\"" m[1] "\""
-  }
-  END {
-    if (n>0) {
-      for (i=1;i<=n;i++) {
-        printf "%s%s", vals[i], (i<n?",":"")
-      }
-      printf "\n"
+awk -v id="some_word" '
+  # once we see <util:set … id="some_word" …>  up to </util:set>...
+  $0 ~ "<util:set[^>]*id=\"" id "\"[^>]*>" , $0 ~ "</util:set>" {
+    # if this line has a <value>…</value>, capture it
+    if (match($0, /<value>([^<]+)<\/value>/, m)) {
+      # print "value", and a comma
+      printf "\"%s\",", m[1]
     }
   }
-' "$XML_FILE"
+  END {
+    # strip trailing comma & print newline
+    sub(/,$/, "")
+    print ""
+  }
+' input.xml
