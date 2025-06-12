05463866eb52ac00db6b311b2c92d0fe9524c9f0
Sridhar Streaks <100800059+sridharstreaks@users.noreply.github.com>
2025-06-13 00:16:04 +0530

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -1,28 +1,24 @@
-word=$(
-  awk '
-    BEGIN {
-      in_block = 0
-      vals     = ""
-    }
-    # 1) opening-tag: must be a <util:set> with id="some_word" anywhere in it
-    /<util:set[^>]*id="some_word"[^>]*>/ {
-      in_block = 1
-      next
-    }
-    # 2) closing-tag: turn it off when you see </util:set>
-    /<\/util:set[[:space:]]*>/ {
-      in_block = 0
-      next
-    }
-    # 3) only inside that block, grab <value>…</value>
-    in_block {
-      if ( match($0, /<value>([^<]+)<\/value>/, m) ) {
-        vals = vals "\"" m[1] "\","
+#!/usr/bin/env bash
+# file: extract_values_simple.sh
+
+XML_FILE=$1
+ID=$2
+
+awk -v id="$ID" '
+  # when we hit the <util:set id"…" line, start collecting…
+  $0 ~ "<util:set id\"" id "\"" { inblock=1; next }
+  # …until the closing tag
+  $0 ~ "</util:set>"      { inblock=0 }
+  # inside block, look for <value>…</value>
+  inblock && match($0, /<value>([^<]+)<\/value>/, m) {
+    vals[++n] = "\"" m[1] "\""
+  }
+  END {
+    if (n>0) {
+      for (i=1;i<=n;i++) {
+        printf "%s%s", vals[i], (i<n?",":"")
       }
+      printf "\n"
     }
-    END {
-      sub(/,$/, "", vals)
-      print vals
-    }
-  ' "$file"
-)
+  }
+' "$XML_FILE"
